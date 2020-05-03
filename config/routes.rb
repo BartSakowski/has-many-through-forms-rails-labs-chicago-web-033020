110fa0944cd6eacf2abde355f6c4a525d2801888
imperfectcastle <bart.sakowski@gmail.com>
2020-05-03 17:39:39 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts, only: [:index, :new, :show, :create]
   resources :comments
-  resources :users
-  resources :categories
+  resources :users, only: [:show]
+  resources :categories, only: [:index, :show]
 end
